e4f0f0f25b52694b5df98c123c70346cbebcc0de
Anastasia Zef <zeff1983@gmail.com>
2024-12-26 22:36:03 +0300

    Tree tests on TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -29,5 +29,15 @@ public class CashbackHackServiceTest {
         assertEquals(expected, actual);
     }
 
-    
+    @Test
+    void shouldNotRemaindIfAmountMoreTheusand() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
 }
